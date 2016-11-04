12eb952acf71997a1d3bd123664cbe99a0ff70af
Lars Ole Simonsen <los@c3a.dk>
2016-11-04 22:41:58 +0100

    The site can now run entirely out of the node server

--- server/api/router.js
+++ server/api/router.js
@@ -16,5 +16,6 @@ module.exports = (app) => {
 
   //Forward other requests to static content
   app.use(express.static(config.STATIC_CONTENT_ROOT));
+  app.use('/node_modules', express.static(config.NODE_MODULES_ROOT));
 
 };
